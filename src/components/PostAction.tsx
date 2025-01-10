6d34481a0ae14acfdd8ec4f09ed1fff285460316
r4ya1n <luzini2007@gmail.com>
2025-01-11 00:26:09 +0500

    realese sortPosts

--- src/components/PostAction.tsx
+++ src/components/PostAction.tsx
@@ -1,16 +1,26 @@
-import { FC } from 'react'
-import { SortedMethods } from '../ENums/SortedMehots'
+import { FC, useState } from 'react'
+import { SortedMethods } from '../ENums/SortedMethods'
 import styles from './PostActionStyles.module.css'
 import { PrButton } from '../UI/prButton/PrButton'
 
-interface IPostActionProps {}
+interface IPostActionProps {
+	sortPosts: (selectedSorting: string) => void
+}
 
-export const PostAction: FC<IPostActionProps> = props => {
+export const PostAction: FC<IPostActionProps> = ({ sortPosts }) => {
+	const [selectedSorting, setSelectedSorting] = useState<string>()
 	return (
 		<div className={styles.actions}>
 			<input placeholder='Поиск...' />
 			<div className={styles.sortAndBtn}>
-				<select name='' id=''>
+				<select
+					value={selectedSorting}
+					onChange={event => {
+						const newValue = event.target.value
+						setSelectedSorting(newValue)
+						sortPosts(newValue)
+					}}
+				>
 					<option value={SortedMethods.byName}>По названию</option>
 					<option value={SortedMethods.byContent}>По контенту</option>
 					<option value={SortedMethods.byTime}>По времени</option>
--- src/components/PostBlock.tsx
+++ src/components/PostBlock.tsx
@@ -3,28 +3,47 @@ import styles from './PostBlockStyles.module.css'
 import { PostAction } from './PostAction.tsx'
 import { PostItem } from './postItem.tsx'
 import type { Post } from '../project'
+import { SortedMethods } from '../ENums/SortedMethods.ts'
 
 export const CommentsBlock: FC = () => {
+	const [selectedSorting, setSelectedSorting] = useState<string>()
 	const [posts, setPosts] = useState<Post[]>([
 		{
 			userId: 1,
 			id: 1,
 			title:
-				'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
-			body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto',
+				'Аunt aut facere repellat provident occaecati excepturi optio reprehenderit',
+			body: 'Бuia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto',
 		},
 		{
 			userId: 1,
 			id: 2,
-			title: 'qui est esse',
-			body: 'est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla',
+			title: 'Бui est esse',
+			body: 'Аst rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla',
 		},
 	])
-
+	const sortPosts = (selectedSorting: string) => {
+		setSelectedSorting(selectedSorting)
+		switch (selectedSorting) {
+			case SortedMethods.byName:
+				setPosts(prevPosts =>
+					[...prevPosts].sort((a, b) => a.title.localeCompare(b.title))
+				)
+				break
+			case SortedMethods.byContent:
+				setPosts(prevPosts =>
+					[...prevPosts].sort((a, b) => a.body.localeCompare(b.body))
+				)
+				break
+			case SortedMethods.byTime:
+				setPosts(prevPosts => [...prevPosts].sort((a, b) => a.id - b.id))
+				break
+		}
+	}
 	return (
 		<div className={styles.block}>
 			<h2 className={styles.header}>Посты</h2>
-			<PostAction />
+			<PostAction sortPosts={sortPosts} />
 			<div className='postsList'>
 				{posts.map(post => {
 					return <PostItem key={post.id} post={post} />
